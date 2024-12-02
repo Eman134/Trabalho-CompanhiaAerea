4f88d6334015ec644440afe4299c83a1fe77f763
Rafael Brant <rafaelrbrant07@gmail.com>
2024-12-02 19:19:42 -0300

    comentario cod anterior
    documentação funções passageiro

--- modulos/Passageiro.h
+++ modulos/Passageiro.h
@@ -1,44 +1,48 @@
 #ifndef PASSAGEIRO_H
 #define PASSAGEIRO_H
-
 #include <string>
 #include <vector>
 
+// Classe Passageiro
 class Passageiro {
 private:
-    int codigo_passageiro;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    bool fidelidade;
+    int codigo_passageiro;      
+    std::string nome;           
+    std::string endereco;      
+    std::string telefone;       
+    bool fidelidade;         
     int pontos_fidelidade;
-
 public:
+     // Construtor com parâmetros para inicializar todos os atributos
     Passageiro(int codigo_passageiro, std::string nome, std::string endereco, std::string telefone, bool fidelidade, int pontos_fidelidade);
+    
+    // Construtor padrão, que inicializa os atributos com valores padrão
     Passageiro();
 
-    int getCodigoPassageiro() const;
-    std::string getNome() const;
-    std::string getEndereco() const;
-    std::string getTelefone() const;
-    bool isFidelidade() const;
+    // Métodos getter para acessar os atributos da classe:
+    int getCodigoPassageiro() const;  
+    std::string getNome() const;      
+    std::string getEndereco() const;  
+    std::string getTelefone() const;  
+    bool isFidelidade() const;        
     int getPontosFidelidade() const;
 
-    void setCodigoPassageiro(int codigo_passageiro);
-    void setNome(std::string nome);
-    void setEndereco(std::string endereco);
-    void setTelefone(std::string telefone);
-    void setFidelidade(bool fidelidade);
-    void setPontosFidelidade(int pontos_fidelidade);
+    // Métodos setter para modificar os atributos da classe:
+    void setCodigoPassageiro(int codigo_passageiro);  
+    void setNome(std::string nome);                    
+    void setEndereco(std::string endereco);            
+    void setTelefone(std::string telefone);            
+    void setFidelidade(bool fidelidade);               
+    void setPontosFidelidade(int pontos_fidelidade); 
 
-    static bool verificarDuplicidade(int codigo_passageiro);
-    static void cadastrarPassageiro();
-    static void salvarPassageiros();
+    // Métodos estáticos da classe, utilizados para manipulação de múltiplos passageiros:
+    static bool verificarDuplicidade(int codigo_passageiro);  
+    static void cadastrarPassageiro(); 
+    static void salvarPassageiros(); 
     static void carregarPassageiros();
-    static Passageiro* buscarPassageiro(int codigo_passageiro);
+    static Passageiro* buscarPassageiro(int codigo_passageiro);  
     static Passageiro* buscarPassageiro(const std::string& nome);
 };
-
+// Declaração de uma variável global que representa a lista de passageiros
 extern std::vector<Passageiro> lista_passageiros;
-
-#endif // PASSAGEIRO_H
+#endif 
